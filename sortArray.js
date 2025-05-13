6429803ec8c3718a86715913ec919b9c337c662d
TylerAycock <tyler.joseph.aycock@gmail.com>
2025-05-13 10:13:58 -0700

    completed daily coding challenge

---
+++ sortArray.js
@@ -0,0 +1,14 @@
+// Given an array of numbers. You have to sort the odd numbers in ascending order while leaving the even numbers at their original positions.
+
+function sortArray(arr) {
+  let odd = arr.filter((num) => num % 2).sort();
+  for (let i = 0; i < arr.length; i++) {
+    if (arr[i] % 2) {
+      arr[i] = odd.shift();
+    }
+  }
+  return arr;
+}
+
+console.log(sortArray([5, 8, 6, 3, 4]));
+console.log(sortArray([5, 3, 2, 8, 1, 4]));
